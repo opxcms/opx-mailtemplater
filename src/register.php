db9bce2e4dd87614d53e7d9e4bd5f6e08f3cfbc6
LozovoyV <lozovoyv@gmail.com>
2019-09-19 15:32:14 +0300

    Move registration to extra

--- src/register.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Modules\Opx\MailTemplater;
-
-return [
-    'provider' => MailTemplater::class,
-    'facade' => OpxMailTemplater::class,
-    'facade_alias' => 'OpxMailTemplater',
-];
